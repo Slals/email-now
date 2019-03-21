e3428ca52190123e84f1696be34c65daa263c188
Jonathan Serra <jonathan@cryptoblocs.fr>
2019-03-21 13:10:33 +0100

    Init commit

--- main.go
+++ main.go
@@ -1,11 +1,38 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
+	"net/smtp"
 	"os"
 )
 
+type Message struct {
+	EmailAddress string `json:"email"`
+	Message      string `json:"message"`
+}
+
 func HandleEmail(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, os.Getenv("SMTP_HOST"))
+	var mess Message
+	decoder := json.NewDecoder(r.Body)
+
+	if err := decoder.Decode(&mess); err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	smtpHost := os.Getenv("SMTP_HOST")
+
+	emailAuth := smtp.PlainAuth("", os.Getenv("SMTP_LOGIN"), os.Getenv("SMTP_PASSWD"), smtpHost)
+
+	to := []string{os.Getenv("EMAIL")}
+	msg := []byte(fmt.Sprintf("%s sent\r\n\r\n%s", mess.EmailAddress, mess.Message))
+
+	if err := smtp.SendMail(smtpHost+os.Getenv("SMTP_PORT"), emailAuth, mess.EmailAddress, to, msg); err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusNoContent)
 }
